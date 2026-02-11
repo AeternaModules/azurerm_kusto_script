96ec945fba87bd2b32a77214f774ad47fea46692
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:53:39 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -7,18 +7,22 @@ Required:
 Optional:
     - continue_on_errors_enabled
     - force_an_update_when_value_changed
+    - principal_permissions_action
     - sas_token
     - script_content
+    - script_level
     - url
 EOT
 
   type = map(object({
     database_id                        = string
     name                               = string
-    continue_on_errors_enabled         = optional(bool, false)
+    continue_on_errors_enabled         = optional(bool) # Default: false
     force_an_update_when_value_changed = optional(string)
+    principal_permissions_action       = optional(string) # Default: "RetainPermissionOnScriptCompletion"
     sas_token                          = optional(string)
     script_content                     = optional(string)
+    script_level                       = optional(string) # Default: "Database"
     url                                = optional(string)
   }))
 }
